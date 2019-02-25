6f1b18a911c2499f83954b45d2f93e93aa4735e8
Sokolov V <sokolov.stb@gmail.com>
2019-02-25 20:02:04 +0300

    feat: add table

---
+++ precache-manifest.3008eb1e3510224db9bbb9818498f489.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "e02e3f4b8094da66e992",
+    "url": "./static/js/main.e02e3f4b.chunk.js"
+  },
+  {
+    "revision": "6adf872fe20990cfc45b",
+    "url": "./static/js/1.6adf872f.chunk.js"
+  },
+  {
+    "revision": "e02e3f4b8094da66e992",
+    "url": "./static/css/main.357efa5d.chunk.css"
+  },
+  {
+    "revision": "2bf484861b2a1d067a8daba0346b175a",
+    "url": "./index.html"
+  }
+];
